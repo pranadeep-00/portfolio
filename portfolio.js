d84f7e5b1e3262da4ed21e195446d2c3c42a0c91
pranadeep-00 <130644838+pranadeep-00@users.noreply.github.com>
2024-02-24 15:11:50 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,17 @@
-function collapse(){
-    
+let header= document.getElementById("header");
+let navbar= document.getElementById("Navbar");
+let Name= document.getElementById("Name");
+let image= document.getElementById("Image");
+let text = document.getElementById("Text");
+
+
+window.addEventListener('scroll',()=>{
+    let value= window.scrollY;
+
+    header.style.marginLeft= value *(-2.5) + 'px';
+    navbar.style.marginRight= value* (-2.5) + 'px';
+    // image.style.marginRight= value*(-2.5)+ 'px';
+    // Name.style.marginLeft= value*(1.5) + 'px';
+    // text.style.marginLeft= value*(1.5) + 'px';
 }
+)
